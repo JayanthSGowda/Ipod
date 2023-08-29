b53b936fec1f630d417908217fdbc6925aa726cc
luffy <jayanth.gowda2807@gmail.com>
2023-08-29 23:43:02 +0530

    getting started

---
+++ src/Components/Screen.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import styles from '../Styles/screen.module.css';
+export default class Screen extends Component {
+  render() {
+    return (
+      <div class="i-screen">
+        
+      </div>
+    );
+  }
+}
